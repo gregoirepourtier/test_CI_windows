b30c8526aedfc1ea1dd770adb9aa52641b44bf9c
gregoirepourtier <gpourtier@icloud.com>
2023-01-17 15:52:31 +0100

    test precompile

--- src/test_CI_windows.jl
+++ src/test_CI_windows.jl
@@ -7,22 +7,25 @@ using ExtendableSparse
 using DifferentialEquations
 
 
-greet() = print("Hello World!")
+greet() = "Hello World!"
 
 
-function ode_test()
-    f(u,p,t) = 1.01*u
-    u0 = 1/2
-    tspan = (0.0,1.0)
-    prob = ODEProblem(f,u0,tspan)
-    sol = solve(prob, Tsit5(), reltol=1e-8, abstol=1e-8)
-
-    return sol
-end
+export greet
 
 
+# function ode_test()
+#     f(u,p,t) = 1.01*u
+#     u0 = 1/2
+#     tspan = (0.0,1.0)
+#     prob = ODEProblem(f,u0,tspan)
+#     sol = solve(prob, Tsit5(), reltol=1e-8, abstol=1e-8)
 
-export ode_test
+#     return sol
+# end
+
+
+
+# export ode_test
 
 
 
--- test/runtests.jl
+++ test/runtests.jl
@@ -2,35 +2,40 @@ using Test
 using test_CI_windows
 using DifferentialEquations
 
-sol1 = ode_test()
-
-
-val_test1 = 0.5
-val_test2 = 1.3728005076225747
-
-
-# @test sol.u[1] ≈ val_test1
-# @test sol.u[end] ≈ val_test2
-
-
-
-function lorenz!(du,u,p,t)
-    du[1] = 10.0*(u[2]-u[1])
-    du[2] = u[1]*(28.0-u[3]) - u[2]
-    du[3] = u[1]*u[2] - (8/3)*u[3]
-end
-
-u0 = [1.0;0.0;0.0]
-tspan = (0.0,100.0)
-prob = ODEProblem(lorenz!,u0,tspan)
-sol2 = solve(prob)
-
-val_test3 = 30.009595859250396
-# @test sum(sol.u[end]) ≈ val_test3
-
 
 @testset begin
-    @test sol1.u[1] ≈ val_test1
-    @test sol1.u[end] ≈ val_test2
-    @test sum(sol2.u[end]) ≈ val_test3
+    @test greet() == "Hello World!"
 end
+
+# sol1 = ode_test()
+
+
+# val_test1 = 0.5
+# val_test2 = 1.3728005076225747
+
+
+# # @test sol.u[1] ≈ val_test1
+# # @test sol.u[end] ≈ val_test2
+
+
+
+# function lorenz!(du,u,p,t)
+#     du[1] = 10.0*(u[2]-u[1])
+#     du[2] = u[1]*(28.0-u[3]) - u[2]
+#     du[3] = u[1]*u[2] - (8/3)*u[3]
+# end
+
+# u0 = [1.0;0.0;0.0]
+# tspan = (0.0,100.0)
+# prob = ODEProblem(lorenz!,u0,tspan)
+# sol2 = solve(prob)
+
+# val_test3 = 30.009595859250396
+# # @test sum(sol.u[end]) ≈ val_test3
+
+
+# @testset begin
+#     @test sol1.u[1] ≈ val_test1
+#     @test sol1.u[end] ≈ val_test2
+#     @test sum(sol2.u[end]) ≈ val_test3
+# end
